c6aa2f9700f98eccf294d361d0a59d9b05d57978
Coreys MacBook Air <coreyjjcunningham@coreycughamsair.fios-router.home>
2016-07-13 12:30:18 -0400

    compressed images

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
-var sprite = require("css-sprite");
+var imageResize = require('gulp-image-resize');
+var imagemin = require("gulp-imagemin");
  
 gulp.task('minify-css', function() {
     return gulp.src(['css/normalize.css','css/foundation.css','css/basics.css'
@@ -23,4 +24,29 @@ gulp.task("minify-js",function(){
 		.pipe(gulp.dest('dist/js'));
 });
 
-gul
+gulp.task("resize",function(){
+    gulp.src(['img/photos/header.jpg', 'img/photos/photo1.jpg','img/photos/photo2.jpg','img/photos/photo2.jpg','img/photos/photo3.jpg',
+        'img/photos/photo4.jpg','img/photos/photo5.jpg','img/photos/photo6.jpg','img/photos/photo7.jpg',
+        'img/photos/photo8.jpg','img/photos/photo9.jpg','img/photos/photo10.jpg'])
+    .pipe(imageResize({
+        width: 800,
+        height: 500
+    }))
+    .pipe(gulp.dest('dist/img/photos'));
+    gulp.src(['img/logo.png']).pipe(imageResize({
+        height: 48,
+        width: 640
+    })).pipe(gulp.dest('dist/'));
+})
+gulp.task('resizeheader',function(){
+    gulp.src(['img/photos/header.jpg']).pipe(imageResize({
+        height: 1440,
+        width: 500,
+        upscale: true,
+        quality: 1
+    })).pipe(gulp.dest('dist/img/photos'))
+});
+
+gulp.task('imagemin',function(){
+    gulp.src('img/avatars/sprite.png').pipe(imagemin()).pipe(gulp.dest('dist/avatars'));
+});
